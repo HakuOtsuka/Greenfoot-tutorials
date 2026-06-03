ce7ac59c09fc3fe4a138e4728b2b0dc8e1f6f26f
BHASVIC-Haku <haku.otsuka25@students.bhasvic.ac.uk>
2026-06-03 17:48:25 +0100

    Add files via upload

---
+++ Greenfield tutorials-4/MyWorld.java
@@ -0,0 +1,39 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 600, 1); 
+        prepare();
+    }
+    public void act(){
+        if(Greenfoot.getRandomNumber(60)<1){
+            addEnemy1();
+        }
+    }
+    public void addEnemy1(){
+        addObject(new Enemy1(),Greenfoot.getRandomNumber(600),0);
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Player player = new Player();
+        addObject(player,94,540);
+        
+    }
+}
